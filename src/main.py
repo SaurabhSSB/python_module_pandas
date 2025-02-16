2e13db059ca0e7cd2e9c36098f5a284fd03e1c89
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-02-16 23:04:35 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# Pandas is a comprehensive library in Python designed for data manipulation and analysis.
+# While it builds upon the capabilities of NumPy, which focuses on numerical operations,
+# Pandas offers extensive functions for handling tabular data, including importing datasets, cleaning data, and analyzing statistics.
+# Its name “Pandas” may reflect its association with bear families or other fictional contexts,
+# but it is rooted in its ability to organize and manipulate data efficiently.
+
+# Key features of Pandas include:
+
+# DataFrame Object: Similar to NumPy’s array, Pandas provides a DataFrame for tabular data storage.
+# Handling Missing Data: It includes functions for managing missing values, such as filling them or marking them as null.
+# Merging Datasets: Pandas allows merging datasets from various sources, which is essential for comprehensive analysis.
+# Time Series Analysis: Specialized tools for handling time series data.
+# Data Visualization: Integration with libraries like Matplotlib and Seaborn for creating insightful visualizations.
+# Pandas is widely used in fields like finance, healthcare, and social sciences due to its versatility in managing diverse data structures
+# and performing complex operations efficiently.
